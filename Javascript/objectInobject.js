4c8f8eeddc67a0a82e2c407cd5d889d051419b20
Swami Sonam Singh <76897479+SwamiSonamSingh@users.noreply.github.com>
2021-05-20 01:34:58 +0530

    Add files via upload

---
+++ Javascript/objectInobject.js
@@ -0,0 +1,15 @@
+var object={
+    name:"Swami Sonam Singh",
+    age:24,
+    locality:"Patna",
+    caste:"Kurmi",
+}
+object.obj1={
+    age:"24",
+};
+if(object.age==object.obj1.age){
+    console.log("True");
+}
+else{
+    console.log("false");
+}
