ecf5fce72bf7c9861a791f66089cd0de039fe722
Swami Sonam Singh <76897479+SwamiSonamSingh@users.noreply.github.com>
2021-05-23 14:38:21 +0530

    Add files via upload

---
+++ randomusergenerater/script.js
@@ -0,0 +1,31 @@
+var button=document.getElementById("btn");
+button.addEventListener("click",function(){
+    var name=document.getElementById("name");
+    var age=document.getElementById("age");
+    var phone=document.getElementById("ph");
+    var mail=document.getElementById("mail");
+    fetch('https://randomuser.me/api/').then(fun=>{
+        return fun.json();
+    }).then((data)=>{
+        var user=data.results[0];
+        name.innerHTML=user.name.title+" "+user.name.first+" "+user.name.last;
+        age.innerHTML=user.dob.age;
+        phone.innerHTML=user.email;
+        mail.innerHTML=user.phone;
+    })
+})
+// setInterval(()=>{
+//     var name=document.getElementById("name");
+//     var age=document.getElementById("age");
+//     var phone=document.getElementById("ph");
+//     var mail=document.getElementById("mail");
+//     fetch('https://randomuser.me/api/').then(fun=>{
+//         return fun.json();
+//     }).then((data)=>{
+//         var user=data.results[0];
+//         name.innerHTML=user.name.title+" "+user.name.first+" "+user.name.last;
+//         age.innerHTML=user.dob.age;
+//         phone.innerHTML=user.email;
+//         mail.innerHTML=user.phone;
+//     })
+// },7000)
