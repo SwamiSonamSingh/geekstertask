c73556c32873ac93fe02763d13b04c6f050238dd
Swami Sonam Singh <76897479+SwamiSonamSingh@users.noreply.github.com>
2021-05-25 12:05:38 +0530

    Add files via upload

---
+++ animated/script.js
@@ -0,0 +1,13 @@
+let glowInTexts = document.querySelectorAll(".animation");
+setInterval(()=>{
+    glowInTexts.forEach(glowInText => {
+        let letters = glowInText.textContent.split("");
+        glowInText.textContent = "";
+        letters.forEach((letter, i) => {
+          let span = document.createElement("span");
+          span.textContent = letter;
+          span.style.animationDelay = `${i * 0.08}s`;
+          glowInText.append(span);
+        });
+      });
+},5000)
