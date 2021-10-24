8508c789105d7f22abda3f24c1e5d2cfbd47d61b
Swami Sonam Singh <76897479+SwamiSonamSingh@users.noreply.github.com>
2021-10-24 11:47:40 +0530

    Add files via upload

---
+++ capstoneproj/script.js
@@ -0,0 +1,7 @@
+let cname=document.getElementById("cname")
+let mailid=document.getElementById("mailid")
+let mob=document.getElementById("mob")
+let button=document.getElementById("aboutbtn")
+button.addEventListener("click",()=>{
+        alert("all field are required")
+})
