5ab103d172efe738d948e8f6a895509d70c20ba1
Swami Sonam Singh <76897479+SwamiSonamSingh@users.noreply.github.com>
2021-10-24 11:48:37 +0530

    Delete script.js

--- capstoneproj/script.js
+++
@@ -1,7 +0,0 @@
-let cname=document.getElementById("cname")
-let mailid=document.getElementById("mailid")
-let mob=document.getElementById("mob")
-let button=document.getElementById("aboutbtn")
-button.addEventListener("click",()=>{
-        alert("all field are required")
-})
